dc6ad223a815bc9398071313f61bb396cd6644b8
Daniel Ruoso <daniel@ruoso.com>
2008-04-06 12:23:49 +0000

    Import of DRUOSO/Catalyst-Controller-SOAP-0.8 from CPAN.
    gitpan-cpan-distribution: Catalyst-Controller-SOAP
    gitpan-cpan-version:      0.8
    gitpan-cpan-path:         DRUOSO/Catalyst-Controller-SOAP-0.8.tar.gz
    gitpan-cpan-author:       DRUOSO
    gitpan-cpan-maturity:     released

---
+++ t/PostApp/lib/PostApp/Controller/WithWSDL4.pm
@@ -0,0 +1,23 @@
+package PostApp::Controller::WithWSDL4;
+
+use strict;
+use warnings;
+use base 'Catalyst::Controller::SOAP';
+
+__PACKAGE__->config->{wsdl} = 't/hello4.wsdl';
+
+sub Greet : WSDLPort('Greet') {
+    my ( $self, $c, $args ) = @_;
+    my $who = $args->{who};
+    my $grt = $args->{greeting};
+    $c->stash->{soap}->compile_return({ greeting => $grt.' '.$who.'!' });
+}
+
+sub Shout : WSDLPort('Greet') {
+    my ( $self, $c, $args ) = @_;
+    my $who = $args->{who};
+    my $grt = $args->{greeting};
+    $c->stash->{soap}->compile_return({ greeting => uc($grt).' '.uc($who).'!' });
+}
+
+1;
